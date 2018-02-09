2a6c6339e3012246c5ed12fcc04bed8f4bbdfd6d
Steve Perella <steve.perella@galvanize.com>
2018-02-09 16:42:20 -0700

    remove .idea/vcs.xml

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -2,8 +2,27 @@ package com.galvanize;
 
 public class Application {
 
+
+    static String grade(String score) {
+        // Implement your solution
+        if (Integer.parseInt(score) >= 90) return("A");
+        else if (Integer.parseInt(score) >= 80) return("B");
+        else if (Integer.parseInt(score) >= 70) return("C");
+        else if (Integer.parseInt(score) >= 60) return("D");
+        else return("F");
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        Application object = new Application();
+
+        System.out.println(grade("95"));
+        System.out.println(grade("85"));
+        System.out.println(grade("75"));
+        System.out.println(grade("65"));
+        System.out.println(grade("55"));
+
     }
 
 }
